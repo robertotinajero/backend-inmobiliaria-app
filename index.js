a32d582bbd5024e6fbfc150c5340c3a30c6db6d4
Roberto Carlos Tinajero Reyes <rtinajero@inprotelecom.com>
2025-08-21 22:18:02 -0600

    CORS policy

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 const corsOptions = {
-    origin: 'https://grupopenainmobiliaria.com',
+    origin: ['http://localhost:5173','https://grupopenainmobiliaria.com'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
@@ -25,7 +25,7 @@ app.get('/health', (req, res) => res.json({ ok: true }));
 // Todas las rutas centralizadas
 app.use('/api', routes);
 
-const port = process.env.PORT || 8080;
+const port = 3000;
 app.listen(port, () => {
   console.log(`Listen on:${process.env.BASE_URL}:${port}`)
 });
